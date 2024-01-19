8df369e7a720d6f1e64e034cd187d9adffb23c60
Luis Fernando <luis.alves>
2024-01-19 16:32:59 -0300

    grading challenge

---
+++ hacker-rank/easy/grading-students.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    
+    arr = []
+    for i in grades:
+        x = i
+        n = 0
+        if i >= 38:
+            while i % 5 != 0:
+                i += 1
+                n += 1
+            if n < 3:
+                x = x + n
+        arr.append(x)
+        
+    return arr
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
