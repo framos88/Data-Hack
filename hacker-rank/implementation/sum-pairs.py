c282d2cd52bd2ada70d21f3abde253f0025a48ca
Luis Fernando <luis.alves>
2024-01-29 16:34:07 -0300

    challenge sum-pairs

---
+++ hacker-rank/implementation/sum-pairs.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'divisibleSumPairs' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER k
+#  3. INTEGER_ARRAY ar
+#
+
+def divisibleSumPairs(n, k, ar):
+    # Write your code here
+    
+    arsoma = []
+    for index, item in enumerate(ar):
+        soma = 0
+        for cont in range(n-1):
+            soma = item + ar[cont+index+1]
+            arsoma.append(soma)
+        n -= 1
+    
+    result = sum(1 for i in arsoma if i % k == 0)
+    
+    return result
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
